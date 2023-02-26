748bcde08f9fad0a04ebc3568b4e48335f81f100
itc-n22003 <n22003@std.it-college.ac.jp>
2023-02-26 17:06:10 +0900

    first

---
+++ JavaScript/fadein.js
@@ -0,0 +1,16 @@
+function fade() {
+    $(window).scroll(function () {
+        $('.fadein').each(function () {
+            const targetElement = $(this).offset().top;
+            const scroll = $(window).scrollTop();
+            const windowHeight = $(window).height();
+            if (scroll > targetElement - windowHeight + 250) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+};
+
+$(window).scroll(function (){
+    fade();
+});
